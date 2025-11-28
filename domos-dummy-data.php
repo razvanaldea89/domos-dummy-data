c0fa6c3dee71fe45f639c4fe8d72862e1acea099
Razvan Aldea <razvan.aldea89@yahoo.ro>
2025-11-28 10:02:29 +0200

    Update domos-dummy-data.php

--- domos-dummy-data.php
+++ domos-dummy-data.php
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * displayDates() is a function, used by the above two functions, that manipulates the dates that we need to set into the tables of our database. This function does not need a calling.
  */
 function add_dlm_dummy_data() {
-	// main branch change.
+	// want to change this to test in the main branch.
 	global $wpdb;
 	$dlms  = get_posts(
 		array(
